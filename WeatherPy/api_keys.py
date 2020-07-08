ea4bf736334db57136a98722c7288738996c5a5f
Charlie Martinez <charlie.mrtnz@gmail.com>
2020-07-08 15:46:15 -0700

    Updating files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "452f1d53f7e04ebd3ca0e70b186edd60"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAzzPpRkqCb3PbzIdtIzOd1VHk02EvmB2o"
